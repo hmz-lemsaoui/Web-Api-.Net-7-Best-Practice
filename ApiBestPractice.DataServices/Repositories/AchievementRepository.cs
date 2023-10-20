c32c3f3340e6ae4d1444c1f851e6473a8418ad47
Hamza Lemsaoui <hamzalemsaoui@gmail.com>
2023-10-20 21:14:20 +0100

    add achievement repo

--- ApiBestPractice.DataServices/Repositories/AchievementRepository.cs
+++ ApiBestPractice.DataServices/Repositories/AchievementRepository.cs
@@ -8,11 +8,77 @@ namespace ApiBestPractice.DataServices.Repositories;
 public class AchievementRepository : GenericRepository<Achievement>, IAchievementRepository
 {
     public AchievementRepository(ILogger logger, AppDbContext context) : base(logger, context)
+    { }
+
+    public async Task<IEnumerable<Achievement>> GetDriverAchievements(Guid driverId)
     {
+        try
+        {
+            return await _dbSet.Where(x => x.DriverId == driverId).ToListAsync();
+        }
+        catch (Exception e)
+        {
+            _Logger.LogError(e, "{Repo} update function error", typeof(AchievementRepository));
+            throw;
+        }
+    }
+    
+    public new async Task<IEnumerable<Achievement>> All()
+    {
+        try
+        {
+            return await _dbSet.Where(x => x.Status == 1)
+                .AsNoTracking()
+                .AsSplitQuery()
+                .OrderBy(x => x.AddedDate)
+                .ToListAsync();
+        }
+        catch (Exception e)
+        {
+            _Logger.LogError(e, "{Repo} all function error", typeof(AchievementRepository));
+            throw;
+        }
     }
 
-    public Task<IEnumerable<Achievement>> GetDriverAchievements(Guid driverId)
+    public new async Task<bool> Delete(Guid id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var result = await _dbSet.FirstOrDefaultAsync(x => x.Id == id);
+            if (result == null) return false;
+
+            result.Status = 0;
+            result.UpdatedDate = DateTime.UtcNow;
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            _Logger.LogError(e, "{Repo} delete function error", typeof(AchievementRepository));
+            throw;
+        }
+    }
+
+    public new async Task<bool> Update(Achievement entity)
+    {
+        try
+        {
+            var result = await _dbSet.FirstOrDefaultAsync(x => x.Id == entity.Id);
+            if (result == null) return false;
+
+            result.DriverId = entity.DriverId;
+            result.FastestLap = entity.FastestLap;
+            result.RaceWins = entity.RaceWins;
+            result.RolePosition = entity.RolePosition;
+            result.WorldChampionship = entity.WorldChampionship;
+            result.UpdatedDate = DateTime.UtcNow;
+            
+            return true;
+        }
+        catch (Exception e)
+        {
+            _Logger.LogError(e, "{Repo} update function error", typeof(AchievementRepository));
+            throw;
+        }
     }
 }
