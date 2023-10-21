c400577ab6b0203cdeb4c9632b7f25f2aa90cd94
Hamza Lemsaoui <hamzalemsaoui@gmail.com>
2023-10-21 11:58:38 +0100

    make config for ef core

---
+++ ApiBestPractice.Api/Configuration/DataBaseConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiBestPractice.Api.Configuration;
+
+public class DataBaseConfig
+{
+    public bool DetailError { get; set; }
+    public int TimeoutTime { get; set; }
+    public bool SensitiveDataLogging { get; set; }
+}
--- ApiBestPractice.Api/Program.cs
+++ ApiBestPractice.Api/Program.cs
@@ -1,3 +1,4 @@
+using ApiBestPractice.Api.Configuration;
 using ApiBestPractice.DataServices.Data;
 using ApiBestPractice.DataServices.Repositories;
 using ApiBestPractice.DataServices.Repositories.Interfaces;
@@ -9,13 +10,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var dbConfig = new DataBaseConfig();
+builder.Configuration.GetSection("DatabaseConfig").Bind(dbConfig);
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddDbContext<AppDbContext>(
-    options => 
-        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options =>
+    {
+        options.UseSqlite(
+            builder.Configuration.GetConnectionString("DefaultConnection"), action =>
+            {
+                action.CommandTimeout(dbConfig.TimeoutTime);
+            });
+        // dev environment
+        options.EnableDetailedErrors(dbConfig.DetailError);
+        options.EnableSensitiveDataLogging(dbConfig.SensitiveDataLogging);
+    });
 
 var app = builder.Build();
 
